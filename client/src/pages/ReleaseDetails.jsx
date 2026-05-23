04a23b060311cbb72b5d654a4b6088a9fa103ab7
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-05-23 21:46:02 +0530

    show

--- client/src/pages/ReleaseDetails.jsx
+++ client/src/pages/ReleaseDetails.jsx
@@ -73,7 +73,7 @@ const ReleaseDetails = () => {
           <div className="flex items-center flex-wrap gap-4 mt-4">
             <button className="flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95 text-white">
               <PlayCircleIcon className="w-5 h-5" />
-              Watch Trailer
+              Coming Soon
             </button>
           </div>
         </div>
